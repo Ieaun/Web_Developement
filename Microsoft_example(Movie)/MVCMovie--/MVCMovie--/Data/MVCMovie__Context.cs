a673357e67456084eaadc9a29c11f0f8a2b83b22
Ieaun <52431013+Ieaun@users.noreply.github.com>
2020-03-24 16:58:09 +0000

    Added completed project

---
+++ Microsoft_example(Movie)/MVCMovie--/MVCMovie--/Data/MVCMovie__Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMovie__.Models;
+
+namespace MVCMovie__.Data
+{
+    public class MVCMovie__Context : DbContext
+    {
+        public MVCMovie__Context (DbContextOptions<MVCMovie__Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMovie__.Models.Movie> Movie { get; set; }
+    }
+}
